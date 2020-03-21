0eac3596915f6043d0a4e1d47c18a6445a422356
Роман Полищуков <romanpolisukov@Mac-Roman.local>
2020-03-21 12:33:24 -0700

    Roma

---
+++ project/project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project
+//
+//  Created by Роман Полищуков on 21.03.2020.
+//  Copyright © 2020 Роман Полищуков. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
